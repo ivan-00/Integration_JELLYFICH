5305bcec5d70dd511536ecd1d7c468dc77e9ae74
ivan-00 <IvanSery0@gmail.com>
2021-01-07 19:27:35 +0100

    lasteversion - JS

--- Dossier exercies/projet/script.js
+++ Dossier exercies/projet/script.js
@@ -8,6 +8,7 @@ let compt           = document.getElementById('app-compt');
 let urlFacture      = ('https://secure.askolga.fr/bucket/40539aa7489a95f9ea63c76a7b0d4c827128dafcd0bac7ebe163183754f0c5195fe072892f50c/search/6477e4ced7ebda88d886bc5b6b2fea24ac90df70d212df60f4cabf31b83db23d5fe1f67733d7a-show.json');
 let urlFrais        = ('https://secure.askolga.fr/bucket/40539aa7489a95f9ea63c76a7b0d4c827128dafcd0bac7ebe163183754f0c5195fe072892f50c/search/217ff136ad3c2a12ad2a022556f2712bda37f108105dc9eaa834df8b37ae17a15fe1f354b5abd-show.json');
 let urlFiche        = ('https://secure.askolga.fr/bucket/40539aa7489a95f9ea63c76a7b0d4c827128dafcd0bac7ebe163183754f0c5195fe072892f50c/search/42b6d6bf16ef570a21777f63600d6ce4543b2c795e94d005bef54b9fa500defc5fe1f62489936-show.json');
+
 // End Url
 function sectionButton(btn){
     for (let j = 0; j< buttons.length; j++){
